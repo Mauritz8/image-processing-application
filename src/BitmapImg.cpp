3714bd4a5b2206eb4ead8bbf586b21d39aecb00d
Mauritz <mauritz.sjodin3@gmail.com>
2023-12-13 18:48:46 +0100

    refactor: use camelcase

--- src/BitmapImg.cpp
+++ src/BitmapImg.cpp
@@ -12,32 +12,32 @@ BitmapImg::BitmapImg(const std::string& filepath)  {
 
     const BitmapHeader bitmapHeader = {
         .identity = contents.substr(0, 2),
-        .nBytes = calc_value(get_byte_values(contents.substr(2, 4))), 
-        .reserved1 = calc_value(get_byte_values(contents.substr(6, 2))),
-        .reserved2 = calc_value(get_byte_values(contents.substr(8, 2))),
-        .offset = calc_value(get_byte_values(contents.substr(10, 4)))
+        .nBytes = calcValue(getByteValues(contents.substr(2, 4))), 
+        .reserved1 = calcValue(getByteValues(contents.substr(6, 2))),
+        .reserved2 = calcValue(getByteValues(contents.substr(8, 2))),
+        .offset = calcValue(getByteValues(contents.substr(10, 4)))
     };
     this->header = bitmapHeader;
 
     const DIBHeader dibHeader = {
-        .dibHeaderSize = calc_value(get_byte_values(contents.substr(14, 4))),
-        .width = calc_value(get_byte_values(contents.substr(18, 4))),
-        .height = calc_value(get_byte_values(contents.substr(22, 4))),
-        .colorPlanes = calc_value(get_byte_values(contents.substr(26, 2))),
-        .bitsPerPixel = calc_value(get_byte_values(contents.substr(28, 2))),
-        .compressionMethod = calc_value(get_byte_values(contents.substr(30, 4))),
-        .imageSize = calc_value(get_byte_values(contents.substr(34, 4))),
-        .horizontalRes = calc_value(get_byte_values(contents.substr(38, 4))),
-        .verticalRes = calc_value(get_byte_values(contents.substr(42, 4))),
-        .nColors = calc_value(get_byte_values(contents.substr(46, 4))),
-        .nImportantColors = calc_value(get_byte_values(contents.substr(50, 4))),
+        .dibHeaderSize = calcValue(getByteValues(contents.substr(14, 4))),
+        .width = calcValue(getByteValues(contents.substr(18, 4))),
+        .height = calcValue(getByteValues(contents.substr(22, 4))),
+        .colorPlanes = calcValue(getByteValues(contents.substr(26, 2))),
+        .bitsPerPixel = calcValue(getByteValues(contents.substr(28, 2))),
+        .compressionMethod = calcValue(getByteValues(contents.substr(30, 4))),
+        .imageSize = calcValue(getByteValues(contents.substr(34, 4))),
+        .horizontalRes = calcValue(getByteValues(contents.substr(38, 4))),
+        .verticalRes = calcValue(getByteValues(contents.substr(42, 4))),
+        .nColors = calcValue(getByteValues(contents.substr(46, 4))),
+        .nImportantColors = calcValue(getByteValues(contents.substr(50, 4))),
     };
     this->dibHeader = dibHeader;
 
     file.close();
 }
 
-int BitmapImg::calc_value(const std::vector<int>& little_endian_bytes) {
+int BitmapImg::calcValue(const std::vector<int>& little_endian_bytes) {
     int sum = 0;
     for (int i = 0; i < little_endian_bytes.size(); i++) {
         sum += little_endian_bytes.at(i) * std::pow(256, i); 
@@ -45,13 +45,13 @@ int BitmapImg::calc_value(const std::vector<int>& little_endian_bytes) {
     return sum;
 }
 
-int BitmapImg::convert_char_to_unsigned_byte(char ch) {
+int BitmapImg::convertCharToUnsignedByte(char ch) {
     return static_cast<int>(static_cast<unsigned char>(ch));
 }
 
-std::vector<int> BitmapImg::get_byte_values(const std::string& str) {
+std::vector<int> BitmapImg::getByteValues(const std::string& str) {
     std::vector<int> values;
     values.resize(str.size());
-    std::transform(str.begin(), str.end(), values.begin(), convert_char_to_unsigned_byte);
+    std::transform(str.begin(), str.end(), values.begin(), convertCharToUnsignedByte);
     return values;
 }
--- src/BitmapImg.hpp
+++ src/BitmapImg.hpp
@@ -14,7 +14,7 @@ class BitmapImg {
         BitmapImg(const std::string& filepath);
 
     private:
-        static int calc_value(const std::vector<int>& little_endian_bytes);
-        static int convert_char_to_unsigned_byte(char ch);
-        static std::vector<int> get_byte_values(const std::string& str);
+        static int calcValue(const std::vector<int>& little_endian_bytes);
+        static int convertCharToUnsignedByte(char ch);
+        static std::vector<int> getByteValues(const std::string& str);
 };
