6e8dd4ac0a3a4d381669cae74cc31b1b2d750924
Mauritz <mauritz.sjodin3@gmail.com>
2023-12-14 23:02:40 +0100

    refactor: change header types from classes to structs

--- src/BitmapHeader.hpp
+++ src/BitmapHeader.hpp
@@ -2,13 +2,11 @@
 #include <string>
 
 
-class BitmapHeader {
-    public:
-        std::string identity;
-        int nBytes;
-        int reserved1;
-        int reserved2;
-        int offset;
-
-        friend std::ostream& operator<<(std::ostream& os, const BitmapHeader& bitmapHeader);
+struct BitmapHeader {
+    std::string identity;
+    int nBytes;
+    int reserved1;
+    int reserved2;
+    int offset;
 };
+std::ostream& operator<<(std::ostream& os, const BitmapHeader& bitmapHeader);
--- src/DIBHeader.hpp
+++ src/DIBHeader.hpp
@@ -1,19 +1,17 @@
 #include <ostream>
 
 
-class DIBHeader {
-    public:
-        int dibHeaderSize;
-        int width;
-        int height;
-        int colorPlanes;
-        int bitsPerPixel;
-        int compressionMethod;
-        int imageSize;
-        int horizontalRes;
-        int verticalRes;
-        int nColors;
-        int nImportantColors;
-
-        friend std::ostream& operator<<(std::ostream& os, const DIBHeader& dibHeader);
+struct DIBHeader {
+    int dibHeaderSize;
+    int width;
+    int height;
+    int colorPlanes;
+    int bitsPerPixel;
+    int compressionMethod;
+    int imageSize;
+    int horizontalRes;
+    int verticalRes;
+    int nColors;
+    int nImportantColors;
 };
+std::ostream& operator<<(std::ostream& os, const DIBHeader& dibHeader);
